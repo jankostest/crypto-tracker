800ed2d290df3a160d8ca4c4a3e0fcf4e1adaee7
Maciej Jankoś <105479868+jestemjankos@users.noreply.github.com>
2026-05-24 21:49:03 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { Home } from './pages/Home';
+import { CoinDetail } from './pages/CoinDetail';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coin/:id" element={<CoinDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
